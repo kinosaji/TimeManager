9772777b4af0a5c0e0b6653e629277e519271971
kinosaji <75092257+kinosaji@users.noreply.github.com>
2021-04-06 01:59:53 +0900

    초기 커밋.

---
+++ Assets/Manager.cs
@@ -0,0 +1,295 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] audioClips;
+
+    AudioSource Audio;
+
+
+    static public int guideNum = 20;
+
+    IEnumerator constNum1() // 수업중
+    {
+        yield return new WaitForSeconds(10f);
+        guideNum = 0;
+    }
+    IEnumerator constNum2() // 쉬는시간
+    {
+        yield return new WaitForSeconds(10f);
+        guideNum = 1;
+    }
+    IEnumerator constNum3() // 점심시간
+    {
+        yield return new WaitForSeconds(10f);
+        guideNum = 2;
+    }
+    private void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+    int Hour = System.DateTime.Now.Hour;
+    int Minute = System.DateTime.Now.Minute;
+    int Second = System.DateTime.Now.Second;
+
+        if (Hour == 9) // 1교시 시작
+        {
+            if (Minute == 20)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 3;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+            }
+
+        }
+        else if (Hour == 10) // 쉬는시간
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 4;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+            }
+        }
+        else if (Hour == 10) // 2교시 시작
+        {
+            if (Minute == 20)
+
+            {
+                if (Second == 00)
+                {
+                    guideNum = 5;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 11) // 쉬는시간
+        {
+            if (Minute == 10)
+            {
+
+                if (Second == 00)
+                {
+                    guideNum = 6;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+            }
+
+        }
+        else if (Hour == 11) // 3교시 시작
+        {
+            if (Minute == 20)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 7;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 12) // 쉬는시간
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 8;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+
+            }
+
+        }
+        else if (Hour == 12) // 4교시 시작
+        {
+            if (Minute == 20)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 9;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 13) // 점심시간 시작
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 10;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum3());
+                }
+
+            }
+
+
+        }
+        else if (Hour == 14) // 쉬는시간
+        {
+            if (Minute == 00)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 11;
+                    Audio.clip = audioClips[2];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+
+            }
+
+
+        }
+        else if (Hour == 14) // 5교시 시작
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 12;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 15) // 쉬는시간
+        {
+            if (Minute == 00)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 13;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+
+            }
+
+        }
+        else if (Hour == 15) // 6교시 시작
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 14;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 16) // 쉬는시간
+        {
+            if (Minute == 00)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 15;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+
+            }
+
+        }
+        else if (Hour == 16) // 7교시 시작
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 16;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 17) // 쉬는시간
+        {
+            if (Minute == 00)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 17;
+                    Audio.clip = audioClips[1];
+                    Audio.Play();
+                    StartCoroutine(constNum2());
+                }
+
+            }
+
+        }
+        else if (Hour == 17) // 8교시 시작
+        {
+            if (Minute == 10)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 18;
+                    Audio.clip = audioClips[0];
+                    Audio.Play();
+                    StartCoroutine(constNum1());
+                }
+
+            }
+
+        }
+        else if (Hour == 18) // 수업종료
+        {
+            if (Minute == 00)
+            {
+                if (Second == 00)
+                {
+                    guideNum = 19;
+                    Audio.clip = audioClips[3];
+                    Audio.Play();
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    TextMesh text;
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+    }
+    void Update()
+    {
+        if (Manager.guideNum == 0)
+        {
+            text.text = "수 업 중";
+        }
+        else if (Manager.guideNum == 1)
+        {
+            text.text = "쉬는시간";
+        }
+        else if (Manager.guideNum == 2)
+        {
+            text.text = "점심시간 (~14시 10분까지)";
+        }
+        else if (Manager.guideNum == 20)
+        {
+            text.text = "탐색중입니다, 잠시만 기다려주세요.";
+        }
+
+
+        else if (Manager.guideNum == 3) // @@@@@@@@@@@@@@수업시작@@@@@@@@@@@@@@
+        {
+            text.text = "1교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 4)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 10시20분";
+        }
+        else if (Manager.guideNum == 5)
+        {
+            text.text = "2교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 6)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 11시20분";
+        }
+        else if (Manager.guideNum == 7)
+        {
+            text.text = "3교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 8)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 12시20분";
+        }
+        else if (Manager.guideNum == 9)
+        {
+            text.text = "4교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 10)
+        {
+            text.text = "지금부터 점심시간입니다.";
+        }
+        else if (Manager.guideNum == 11)
+        {
+            text.text = "곧 점심시간이 끝납니다. \r\n다음 수업시작시간 : 14시10분";
+        }
+        else if (Manager.guideNum == 12)
+        {
+            text.text = "5교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 13)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 15시10분";
+        }
+        else if (Manager.guideNum == 14)
+        {
+            text.text = "6교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 15)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 16시10분";
+        }
+        else if (Manager.guideNum == 16)
+        {
+            text.text = "7교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 17)
+        {
+            text.text = "지금부터 쉬는시간입니다. \r\n다음 수업시작시간 : 17시10분";
+        }
+        else if (Manager.guideNum == 18)
+        {
+            text.text = "8교시 시작을 알립니다.";
+        }
+        else if (Manager.guideNum == 19)
+        {
+            text.text = "금일 모든 수업이 종료되었습니다.";
+        }
+    }
+}
